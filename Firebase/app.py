40cea5c68ec2c672eae1a871494f2667a487f27a
Hekmat Hamidi <theonehamidi@gmail.com>
2019-07-03 19:37:47 -0400

    tested firebase realtime database

---
+++ Firebase/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyCrsmE2GjJRA0OmijMastQ5fUuH8EAu5pc",
+    "authDomain": "innate-star-228407.firebaseapp.com",
+    "databaseURL": "https://innate-star-228407.firebaseio.com",
+    "projectId": "innate-star-228407",
+    "storageBucket": "innate-star-228407.appspot.com",
+    "messagingSenderId": "130872333529",
+    "appId": "1:130872333529:web:1fdab67c0dff6a99"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+#https://www.youtube.com/watch?v=aojoWWMN1r0
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        db.child('todo').push(name)
+        todo = db.child('todo').get()
+        to = todo.val()
+        return render_template('index.html', t=to.values())
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
