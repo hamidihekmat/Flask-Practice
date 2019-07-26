c3541eccda01222a6eef9a1ed6900d456d7e1c38
Hekmat Hamidi <theonehamidi@gmail.com>
2019-07-26 00:47:47 -0400

    added jwt token

--- Sessions/app.py
+++ Sessions/app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env3 python3
 
-from flask import Flask, session, request, render_template, url_for, redirect
-
+from flask import Flask, session, request, render_template, url_for, redirect, flash
+from os import urandom
+import jwt
 
 app = Flask(__name__)
-app.secret_key = b'_53y2L"F4Q8z\n\xec]/'
+app.secret_key = urandom(24)
 
 
 @app.route('/', methods=['Get', 'Post'])
@@ -13,8 +14,11 @@ def home():
         session.pop('user', None)
         password = request.form['password']
         if password == 'hekmat':
-            session['user'] = 'hekmat'
+            session['user'] = jwt.encode({'name': 'user'}, app.secret_key)
             return redirect(url_for('protected'))
+        if password != 'hekmat':
+            flash('You\'ve entered the wrong password!', 'error')
+            return redirect(url_for('home'))
     if request.method == 'GET' and 'user' in session:
         return '<h1>Already Logged In</h1>'
 
@@ -23,9 +27,12 @@ def home():
 
 @app.route('/protected')
 def protected():
-    if 'user' in session:
+    try:
+        jwt.decode(session['user'], app.secret_key)
         return session['user']
-    return 'Your are not logged in!'
+    except:
+        return '<h1>You are not authorized</h1>'
+    return '<h1>Your are not logged in!</h1>'
 
 
 @app.route('/logout')
@@ -39,3 +46,4 @@ if __name__ == '__main__':
 
 
 # still need to fix everything
+# implement SQL database with users and registration
